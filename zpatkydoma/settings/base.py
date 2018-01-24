314a81f9eedcf0a9c3925894694fb9488184c029
jan Cerny <jan.cerny@telegraph.co.uk>
2018-01-24 16:09:09 +0000

    structured data

--- zpatkydoma/settings/base.py
+++ zpatkydoma/settings/base.py
@@ -143,3 +143,6 @@ WAGTAIL_SITE_NAME = "zpatkydoma"
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 BASE_URL = 'http://example.com'
+
+
+TAGGIT_CASE_INSENSITIVE =True
