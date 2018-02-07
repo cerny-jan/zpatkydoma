19c3da2895afdab7f9feb4df0d014840611870a5
jan Cerny <jan.cerny@telegraph.co.uk>
2018-02-07 14:49:30 +0000

    custom context processor added to staging and prod settings

--- zpatkydoma/settings/production.py
+++ zpatkydoma/settings/production.py
@@ -62,6 +62,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'wagtail.contrib.settings.context_processors.settings',
+                'zpatkydoma.base.context_processors.custom_settings',
             ],
             'loaders': [
                 ('django.template.loaders.cached.Loader', [
