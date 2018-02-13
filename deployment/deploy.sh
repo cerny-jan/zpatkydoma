7b23653f57b7f9828399062315d1fdc575694c1a
cerny-jan <xcerj107@gmail.com>
2018-02-13 22:44:40 +0000

    deployment script updated

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -10,10 +10,11 @@ fi
 
 # Decrypting private key
 openssl aes-256-cbc -K $encrypted_f2ef1a2b0bb8_key -iv $encrypted_f2ef1a2b0bb8_iv -in deployment/zpatkydoma_deployment.enc -out deployment/zpatkydoma_deployment -d
+chmod 600 deployment/zpatkydoma_deployment
 
 # Run the 'update.sh' script remotely.
 ssh 'app@node-14.rosti.cz' -p '14232' -o 'StrictHostKeyChecking no' -i 'deployment/zpatkydoma_deployment' '/srv/app/deployment/update.sh'
 
 
 # Remove decrypted private key
-rm deployment/id_rsa_pyvec_deployment
+rm deployment/zpatkydoma_deployment
