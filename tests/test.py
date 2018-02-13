6a45ef6e58000ef3bd33fce67c2bcdee7f4e2cf7
cerny-jan <xcerj107@gmail.com>
2018-02-13 21:54:19 +0000

    travis file updated, test typo fixed

--- tests/test.py
+++ tests/test.py
@@ -21,15 +21,15 @@ class ZpatkydomaTest(TestCase):
         self.assertEqual(blog_page_with_2.related_pages_columns(), 6)
 
     def test_blog_tag_page(self):
-        blog_tagt_page = BlogTagPage.objects.get(slug='tags')
+        blog_tag_page = BlogTagPage.objects.get(slug='tags')
         # test tag page without passing any tag
-        response = self.client.get(blog_tagt_page.url)
+        response = self.client.get(blog_tag_page.url)
         self.assertEqual(response.status_code, 200)
         self.assertIsNone(response.context['searched_tag'])
         self.assertEqual(len(response.context['blogpages']), 3)
         # test tag page passing tag 'english'
         tag = 'english'
-        response = self.client.get(blog_tagt_page.url, {'tag': tag})
+        response = self.client.get(blog_tag_page.url, {'tag': tag})
         self.assertEqual(response.status_code, 200)
         # this should test friendly tag name
         # self.assertContains('english', response.context['searched_tag'])
