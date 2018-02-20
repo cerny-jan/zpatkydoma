3cae74d8c31a663dd6200a34586a98cd5234d709
jan Cerny <jan.cerny@telegraph.co.uk>
2018-02-20 10:23:06 +0000

    smtp switched to zoho

--- zpatkydoma/settings/staging.py
+++ zpatkydoma/settings/staging.py
@@ -98,10 +98,11 @@ DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.zoho.eu'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER')
 
 
 TRACKING_ENVIRONMENT = 'staging'
