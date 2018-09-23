ec29927da861cc1dcdd28e13c9437179a6b6f790
cerny-jan <xcerj107@gmail.com>
2018-09-23 09:19:51 +0200

    django logging updated

--- zpatkydoma/settings/base.py
+++ zpatkydoma/settings/base.py
@@ -217,13 +217,17 @@ LOGGING = {
         'console': {
             'class': 'logging.StreamHandler',
         },
+        'file': {
+            'class': 'logging.FileHandler',
+            'filename': '/srv/logs/django/debug.log',
+        },
         'mail_admins': {
             'class': 'django.utils.log.AdminEmailHandler',
         }
     },
     'loggers': {
         'django': {
-            'handlers': ['console'],
+            'handlers': ['console', 'file'],
             'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
         },
         'django.request': {
